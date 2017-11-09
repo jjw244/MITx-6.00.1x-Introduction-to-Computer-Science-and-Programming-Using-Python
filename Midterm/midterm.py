9a538f35dd86cdde5a17551e3752f1cba4f98a6a
Jennifer washburn <jjw244@users.noreply.github.com>
2017-11-09 17:38:15 -0600

    Midterm

---
+++ Midterm/midterm.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 10:15:24 2017
+
+course: MITx 6.00.1x: Introduction to Computer Science and Programming Using Python
+source: https://courses.edx.org/courses/course-v1:MITx+6.00.1x+2T2017
+"""
+
+"""
+Midterm - Coming Soon - I didn't take the midterm and still PASSED the class :)
+
+Instructions:
+    
+
+"""
+
+
+"""Solution"""
